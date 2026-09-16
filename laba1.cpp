d2dff6183caeb33f15dc62ee43d440eabea8df67
Kilcelsnpl2 <kirillkarpov464@gmail.com>
2026-09-16 17:09:55 +0700

    +CPP

---
+++ laba1.cpp
@@ -0,0 +1,26 @@
+﻿/*************************
+ * Автор:	 Карпов К.Е. *
+ * Дата:	 28.12.2007  *
+ * Название: вариант 4   *
+ *************************/
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main() 
+{
+  double n1, b, K, y;
+  cout << " b = ";
+  cin >> b;
+  cout << " y = ";
+  cin >> y;
+	
+  K = (y - 1) / y;
+  
+  n1 = 1 - (pow((1 / b), K));
+  
+  cout << "n1 =" << n1 << endl;
+  
+  return 0;
+}
